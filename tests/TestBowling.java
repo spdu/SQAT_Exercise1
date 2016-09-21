5c4d40de5a3bbd95d0dd8109786bc5ba67ed5c5f
somename <someemail>
2016-09-21 15:10:55 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -10,7 +10,7 @@ public class TestBowling {
 	 
 	 
 	 
-	 assertEquals(23, frame.getFirstThrow + frame.getSecondThrow()());
+	 assertEquals(2,3, frame.getFirstThrow() + frame.getSecondThrow());
 	}
 
 }
