f9f8fb80686c7aaab6883ef8df1ac7c4370217d9
somename <someemail>
2016-09-21 16:15:38 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -77,5 +77,32 @@ public class TestBowling {
 	 assertEquals(95, game.score());
 
 	}
+	@Test
+	public void testGame_10frames_spare() {
+	BowlingGame game = new BowlingGame();
+	Frame frame1 = new Frame(1,9);
+	 game.addFrame(frame1);
+	 Frame frame2 = new Frame(4,6);
+	 game.addFrame(frame2);
+	 Frame frame3 = new Frame(7,3);
+	 game.addFrame(frame3);
+	 Frame frame4 = new Frame(3,6);
+	 game.addFrame(frame4);
+	 Frame frame5 = new Frame(4,4);
+	 game.addFrame(frame5);
+	 Frame frame6 = new Frame(5,3);
+	 game.addFrame(frame6);
+	 Frame frame7 = new Frame(3,3);
+	 game.addFrame(frame7);
+	 Frame frame8 = new Frame(4,5);
+	 game.addFrame(frame8);
+	 Frame frame9 = new Frame(8,1);
+	 game.addFrame(frame9);
+	 Frame frame10 = new Frame(2,6);
+	 game.addFrame(frame10);
+	 
+	 assertEquals(95, game.score());
+
+	}
 
 }
